6395c7c8afcbacd97d2ebcbab2bf481a53d8b945
Rycs123 <riyanda.c.sinambela@gmail.com>
2024-03-01 23:59:53 +0700

    Added the Task and IsDone components to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import IsDone from "./containers/IsDone/IsDone";
+import Task from "./containers/Task/Task";
+
 function App() {
-    return <div></div>;
+    return (
+        <>
+            <h1 className="title-app">Todo's Riyanda</h1>
+            <div className="border-outer">
+                <div className="board">
+                    <Task />
+                    {/* <IsDone /> */}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
